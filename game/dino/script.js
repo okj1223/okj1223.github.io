00a00bcb76f482e552629c8ef35d677a5f88179e
okj1223 <rudwns181211@gmail.com>
2025-07-24 15:09:46 +0900

    Update script.js

--- game/dino/script.js
+++ game/dino/script.js
@@ -1,1 +1,34 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 
+document.addEventListener("keydown", function(event) {
+  jump();
+});
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
+
+let isAlive = setInterval(function () {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+  if (cactusLeft < 70 && cactusLeft > 0 && dinoTop >= 140) {
+    alert("Game Over!");
+  }
+}, 10);
+
+setInterval(function () {
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+  if (cactusLeft <= -20) {
+    cactus.style.left = "600px";
+  } else {
+    cactus.style.left = cactusLeft - 10 + "px";
+  }
+}, 50);
